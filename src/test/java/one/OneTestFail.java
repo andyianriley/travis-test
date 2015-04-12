e0aa1f2a0f5831b0cedbb9fa11a2e37b2b05c230
andy.riley <andy.riley@tradermedia.co.uk>
2015-04-12 17:32:00 +0100

    force a fail

---
+++ src/test/java/one/OneTestFail.java
@@ -0,0 +1,15 @@
+package one;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class OneTestFail {
+
+    @Test
+    public void testName() throws Exception {
+        assertThat(new One().hello(),is(false));
+
+    }
+}
